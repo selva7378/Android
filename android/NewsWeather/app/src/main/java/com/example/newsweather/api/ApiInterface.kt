c7ee17818771891b7cd7e662462ccec16a74f128
selvaganesh <selvaganesh7378@gmail.com>
2024-05-14 09:10:18 +0530

    Revert "db inspection gone"
    This reverts commit 6574dfc38256b8f2c1ae170efd4def3159ada4fb.

--- android/NewsWeather/app/src/main/java/com/example/newsweather/api/ApiInterface.kt
+++ android/NewsWeather/app/src/main/java/com/example/newsweather/api/ApiInterface.kt
@@ -1,7 +1,9 @@
-package com.example.newsweather.api.news
+package com.example.newsweather.api
 
+import com.example.newsweather.database.roomdb.News
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface ApiInterface {
--- android/NewsWeather/app/src/main/java/com/example/newsweather/api/weather/Weather.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newsweather.api.weather
-
-data class Weather(
-    val `data`: Data,
-    val location: Location
-)
