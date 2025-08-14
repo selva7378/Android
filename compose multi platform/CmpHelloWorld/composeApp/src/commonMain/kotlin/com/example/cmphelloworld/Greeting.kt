c9edd933b7a5fee58d0e5914df521cb40f3e9ee5
Selvaganesh M <selva@Selvaganeshs-MacBook-Air.local>
2025-08-14 09:13:25 +0530

    started compose multiplatform project

---
+++ compose multi platform/CmpHelloWorld/composeApp/src/commonMain/kotlin/com/example/cmphelloworld/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.cmphelloworld
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ compose multi platform/CmpHelloWorld/composeApp/src/commonTest/kotlin/com/example/cmphelloworld/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.cmphelloworld
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
